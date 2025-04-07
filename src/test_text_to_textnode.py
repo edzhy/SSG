a6ca66d287db3a6b38b22c871b8106da3164d838
Edgars <the1edgars@gmail.com>
2025-04-07 06:53:21 +0300

    finished inline functionality

--- src/test_text_to_textnode.py
+++ src/test_text_to_textnode.py
@@ -23,3 +23,19 @@ class TestTextToTextNode(unittest.TestCase):
                 TextNode("link", TextType.LINK, "https://boot.dev"),
                 ]
         )   
+#2 testing adjacent inline elements
+    def test_adjacent(self):
+        test_list = [TextNode("Just checking if **big bold meets**_italic text from heaven_", TextType.TEXT),]
+        result = text_to_textnodes(test_list)
+        self.assertListEqual(result,
+                             [TextNode("Just checking if ", TextType.TEXT),
+                            TextNode("big bold meets", TextType.BOLD),
+                            TextNode("italic text from heaven", TextType.ITALIC)]
+                             )
+#3 testing just text
+    def test_just_text(self):
+        test_list = [TextNode("Just checking if nothing really happens", TextType.TEXT),]
+        result = text_to_textnodes(test_list)
+        self.assertListEqual(result,
+                             [TextNode("Just checking if nothing really happens", TextType.TEXT)]
+                             )
